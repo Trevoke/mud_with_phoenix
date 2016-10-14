2acc2fd6c3f88c84fd7ca6fbaa7aa431a3cd26fc
Aldric Giacomoni <trevoke@gmail.com>
2016-10-13 20:06:43 -0400

    dev db config

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :mud, Mud.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "mud_phoenix",
+  password: "",
   database: "mud_dev",
   hostname: "localhost",
   pool_size: 10
